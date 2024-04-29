b2ad8b9f0fb368e107f6b3d00c011a4884987e10
Dillon Kennamer <156338809+m263288@users.noreply.github.com>
2024-04-29 11:33:27 -0400

    removed modify button

--- mainscript.js
+++ mainscript.js
@@ -2,20 +2,5 @@ document.getElementById("myButton").addEventListener("click", function() {
     alert("Button clicked!");
   });
 
-  document.getElementById("modifyButton").addEventListener("click", function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "modifyFile.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          alert(xhr.responseText); // Display response from server
-        } else {
-          alert("Error: " + xhr.status);
-        }
-      }
-    };
-    xhr.send();
-  });
 
-  
+
